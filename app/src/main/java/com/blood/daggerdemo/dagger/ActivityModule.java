719f43371c04060b14fd8963183e104d20fed028
bloodsoul <812566160@qq.com>
2020-12-23 10:04:06 +0800

    commit dagger-android

--- app/src/main/java/com/blood/daggerdemo/dagger/ActivityModule.java
+++ app/src/main/java/com/blood/daggerdemo/dagger/ActivityModule.java
@@ -8,6 +8,8 @@ import dagger.android.ContributesAndroidInjector;
 @Module
 public abstract class ActivityModule {
 
+    // 标记哪个类需要使用依赖注入功能
+    // 帮我们生成需要的Subcomponent
     @ContributesAndroidInjector
     abstract MainActivity contributeMainActivity();
 
